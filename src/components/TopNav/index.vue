b8ea07decb203536204806c1cc733b588531f56f
stanislavskiy <stanislavskiy.sanya@gmail.com>
2018-06-26 06:47:36 +0300

    Top nav

---
+++ src/components/TopNav/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="topNav" class="top-nav">
+    <img src="" alt="img" class="top-nav__logo">
+    <div class="top-nav__menu">
+      menu
+    </div>
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: "header"
+// };
+</script>
