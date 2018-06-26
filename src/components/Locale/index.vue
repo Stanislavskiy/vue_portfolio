56713c27c57a7f103ddebb671bcd32a78d757fa6
stanislavskiy <stanislavskiy.sanya@gmail.com>
2018-06-26 06:47:51 +0300

    Locale

---
+++ src/components/Locale/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="locale" class="locale">
+    <a href="#" class="locale__button">RU</a>
+    <a href="#" class="locale__button">EN</a>
+    <a href="#" class="locale__button">PL</a>
+  </div>
+</template>
